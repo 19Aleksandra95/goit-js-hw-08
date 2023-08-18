0623806bcc533cf54e57c5a7446f934a294700ce
19Aleksandra95 <polar.heder@gmail.com>
2023-08-18 12:59:36 +0200

    third change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,60 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+console.log('galleryItems', galleryItems);
 // Change code below this line
-
 
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
 console.log(galleryItems);
 
-const galleryList = document.querySelector('.gallery');
+const galleryListRefs = document.querySelector('.gallery');
+const markapList = galleryItems
+  .map(img => {
+    return `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`;
+  })
+  .join('');
+galleryListRefs.innerHTML = markapList;
 
-const markupList = galleryItems
-.map(img => {
-    return `<div class="gallery__item">
-    <a class="gallery__link" href="${img.original}">
-      <img
-        class="gallery__image"
-        src= "${img.preview}"
-        data-source="${img.original}"
-        alt="${img.description}"
-      />
-    </a>
-  </div>`;
-})
-.join('');
-galleryList.innerHTML = markupList;
-galleryList.addEventListener('click', openModal);
+galleryListRefs.addEventListener('click', openModal);
 
-function openModal(event){
-    event.preventDefault();
-        if(!event.target.classList.contains('gallery__image')){
-            return;
-        }
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
 
-        const instance = basicLightbox.create(
-            `<img src="${event.target.dataset.source}" width="800" height="600">`,
-            {
-              onShow: instance => {
-                window.addEventListener('keydown', closeModal);
-              },
-              onClose: instance => {
-                window.removeEventListener('keydown', closeModal);
-              },
-            }
-          );
-          function closeModal(event) {
-            if (event.code === 'Escape') {
-                instance.close();
-            }
-          }
-          instance.show();
-        }
-
-        const lightbox = new SimpleLightbox('.gallery a', {
-            captionDelay: 250,
-            captionsData: 'alt',
-          });
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
